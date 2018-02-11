3e256cebb6be7d5e4d05f9606399d2c41b71541c
RealStrategy <36258164+RealStrategy@users.noreply.github.com>
2018-02-10 22:33:35 -0500

    Add files via upload

---
+++ Sandia.sh
@@ -0,0 +1,166 @@
+#!/data/data/com.termux/files/usr/bash
+#Created on 11/01/2017 by RS
+
+clear
+echo "***CARGANDO AMOR POR M"
+sleep 2
+clear
+echo "***CARGANDO AMOR POR MI ."
+sleep 2
+clear
+echo "***CARGANDO AMOR POR MIR . ."
+sleep 2
+clear
+echo "***CARGANDO AMOR POR MIRE . . ."
+sleep 2
+clear
+echo "***CARGANDO AMOR POR MIREL . . . . "
+sleep 2
+clear
+echo "***CARGANDO AMOR POR MIRELL . . . . ."
+sleep 2
+clear
+echo "***CARGANDO AMOR POR MIRELLA . . . . . ."
+sleep 2
+clear
+echo "***DESCARGA COMPLETADA***"
+sleep 2
+clear
+echo "***CORAZON DE LIAM COMPLETADO*** "
+echo
+apt update && apt upgrade -y
+apt install -y figlet util-linux
+clear
+
+#Special thanks
+setterm -foreground cyan
+	echo "#########################################";
+setterm -foreground magenta 
+	echo "GRACIAS A TI, MI CORAZÓN VOLVIO A RENACER";
+setterm -foreground cyan
+	echo "#########################################";
+	sleep 5
+
+   echo
+setterm -foreground green
+
+ #BANNER
+	echo
+        echo "=========================================";
+figlet Real
+figlet Strategy
+        echo "=========================================";
+sleep 4
+setterm -foreground yellow
+       echo " MESSAGE"
+setterm -foreground red
+        echo ""
+        echo "[LIAM Y MIRELLA]"
+        echo "[SIEMPRE]"
+        echo "[JUNTOS EN EL]"
+        echo "[AMOR]"
+        echo 
+setterm -foreground yellow
+        echo "Choose your Message"
+	echo "1) Recibir Mensaje"
+        echo
+        read -p "R= " answer
+        clear
+	echo
+
+case $answer in
+
+	1)
+setterm -foreground yellow
+                echo
+		echo "Al Utilizar E Instalar Esta Herramienta Significa Que Estas De Acuerdo En :"
+setterm -foreground green
+	echo ""
+	echo "[1] Cuidarlo Mucho y darle cariño"
+	echo "[2] Estar con el en las buenas y malas"
+	echo "[3] Respetarse mutuamente "
+   echo "[4] Reír juntos y hacer locuras buenas "
+   echo "[5] Ser felices por siempre "
+	echo " | "
+	echo "[!] Recuerda , La comunicación entre ustedes es Requerido" 
+	echo
+setterm -foreground blue
+		echo "-----------------------------------------------------------------------------------"
+		echo "Estas Seguro De Querer Instalar EL AMOR DE LIAM ?"
+		echo "-----------------------------------------------------------------------------------"
+		echo
+setterm -foreground white
+		echo "Presiona ENTER si estas de acuerdo"
+		echo "                                     ó"
+		echo "Presiona CTRL + C si no estas de acuerdo"
+		read ENTER
+		clear
+echo
+setterm -foreground green
+figlet te
+sleep 3
+setterm -foreground yellow
+figlet amo
+sleep 3
+setterm -foreground green
+figlet mucho
+sleep 3
+setterm -foreground yellow
+figlet mirella
+sleep 3
+setterm -foreground green
+figlet no
+sleep 3
+setterm -foreground yellow
+figlet te
+sleep 3
+setterm -foreground green
+figlet olvides
+sleep 3
+setterm -foreground yellow
+figlet nunca
+sleep 3
+clear
+echo
+setterm -foreground magenta
+		echo "***Liam iniciara la instalacion del amor"
+		sleep 1
+                clear
+                echo "***Liam iniciara la instalacion del amor."
+                sleep 1
+		clear
+		echo "***Liam iniciara la instalacion del amor . ."
+                sleep 1
+                clear
+                echo "***Liam iniciara la instalacion del amor. . ."
+                sleep 1
+setterm -foreground white
+                pkg install cmatrix
+                echo
+setterm -foreground magenta
+                echo "Liam Esta Descargando y  Extrayendo . . ."
+setterm -foreground white
+       echo
+setterm -foreground yellow
+	clear
+	echo " >>>>>>>>>>> NO OLVIDES QUE TE AMO <<<<<<<<<<<<<"
+setterm -foreground red
+	figlet -f banner LOVE
+   echo
+setterm -foreground yellow
+	echo " >>>>>>>>>>>>>> SIEMPRE PARA TI<<<<<<<<<<<<<<<<<"
+setterm -foreground blue
+	echo
+	echo " ### AHORA ESCRIBEME Y DIME QUE ME AMAS ###"
+   echo " ### AHORA ESCRIBEME Y DIME QUE ME AMAS ###"
+   echo " ### AHORA ESCRIBEME Y DIME QUE ME AMAS ###"
+	sleep 3	
+echo "
+
+	"
+setterm -foreground green
+	echo "LISTO YA PUEDES CORRER LAS ESTRELLAS SOLO EJECUTANDO EL COMANDO :=> cmatrix"
+	echo "
+	"
+setterm -foreground white
+	esac
